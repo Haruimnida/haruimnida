f7d3287d1d49d6de2f8b5a7b887f962e44762194
Haruimnida <harushiitorredes@gmail.com>
2025-06-21 09:35:42 +0800

    Update _HEART html_.js

--- _HEART html_.js
+++ _HEART html_.js
@@ -1,113 +1,113 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Heart Particles</title>
-    <style>
-        body {
-            margin: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            background-color: #262626;
-        }
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Heart Particles</title>
+  <style>
+    body {
+      margin: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      background-color: #262626;
+    }
+ 
+    canvas {
+      display: block;
+    }
+   </style>
+ </head>
 
-        canvas {
-            display: block;
-        }
-    </style>
-  </head>
-
-  <body>
+ <body>
     <!-- credits to the writter @rivalfarizi follow him in https://codepen.io/rivalfarizi/pen/zxOWLry-->
-     <canvas id="pinkboard" style="width: 100%; height: 100%;"></canvas>
-     <script>
-        var settings = {
-            particles: {
-                length: 10000,
-                duration: 4,
-                velocity: 80,
-                effect: -1.3,
-                size: 8,
-            },
-        };
-        (function() {
-            var b = 0;
-            var c = ["ms", "moz", "webkit", "o"];
-            for (var a = 0; a < c.length && !window.requestAnimationFrame; ++a) {
-                window.requestAnimationFrame = window[c[a] + "RequestAnimationFrame"];
-                window.cancelAnimationFrame = 
-                  window[c[a] + "CancelAnimationFrame"] || 
-                  window[c[a] + "CancelRequestAnimationFrame"];
-            }
-            if (!window.requestAnimationFrame) {
-                window.requestAnimationFrame = function(h, e) {
-                    var d = new Date().getTime();
-                    var f = Math.max(0, 16 - (d - b));
-                    var g = window.setTimeout(function() {
-                        h(d + f);
-                    }, f);
-                    b = d + f;
-                    return g;
-                };
-            }
-            if (!window.cancelAnimationFrame) {
-                window.cancelAnimationFrame = function(d) {
-                    clearTimeout(d);
-                };
-            }
+    <canvas id="pinkboard" style="width: 100%; height: 100%;"></canvas>
+    <script>
+      var settings = {
+        particles: {
+          length: 10000,
+          duration: 4,
+          velocity: 80,
+          effect: -1.3,
+          size: 8,
+        },
+      };
+      (function() {
+        var b = 0;
+        var c = ["ms", "moz", "webkit", "o"];
+        for (var a = 0; a < c.length && !window.requestAnimationFrame; ++a) {
+          window.requestAnimationFrame = window[c[a] + "RequestAnimationFrame"];
+          window.cancelAnimationFrame = 
+            window[c[a] + "CancelAnimationFrame"] || 
+            window[c[a] + "CancelRequestAnimationFrame"];
+        }
+        if (!window.requestAnimationFrame) {
+          window.requestAnimationFrame = function(h, e) {
+            var d = new Date().getTime();
+            var f = Math.max(0, 16 - (d - b));
+            var g = window.setTimeout(function() {
+              h(d + f);
+            }, f);
+            b = d + f;
+            return g;
+          };
+         }
+         if (!window.cancelAnimationFrame) {
+            window.cancelAnimationFrame = function(d) {
+              clearTimeout(d);
+            };
+          }
         })();
         var Point = (function() {
-            function Point(x, y) {
-                this.x = typeof x !== "undefined" ? x : 0;
-                this.y = typeof y !== "undefined" ? y : 0;
+          function Point(x, y) {
+            this.x = typeof x !== "undefined" ? x : 0;
+            this.y = typeof y !== "undefined" ? y : 0;
+          }
+          Point.prototype.clone = function() {
+            return new Point(this.x, this.y);
+          };
+          Point.prototype.length = function(length) {
+            if (typeof length == "undefined") {
+                return Math.sqrt(this.x * this.x + this.y * this.y);
             }
-            Point.prototype.clone = function() {
-                return new Point(this.x, this.y);
-            };
-            Point.prototype.length = function(length) {
-                if (typeof length == "undefined") {
-                    return Math.sqrt(this.x * this.x + this.y * this.y);
-                }
-                this.normalize();
-                this.x *=length;
-                this.y *=length;
-                return this;
-            };
-            Point.prototype.normalize = function() {
-                var length = this.length();
-                this.x /= length;
-                this.y /= length;
-                return this;
-            };
-            return Point;
-        })();
-        var Particle = (function() {
-            function Particle() {
-                this.position = new Point();
-                this.velocity = new Point();
-                this.acceleration = new Point();
-                this.age = 0;
-            }
-            Particle.Prototype.initialize = function(x, y, dx, dy) {
-                this.position.x = x;
-                this.position.y = y;
-                this.velocity.x = dx;
-                this.velocity.y = dy;
-                this.acceleration.x = dx * settings.particles.effect;
-                this.acceleration.y = dy * settings.particles.effect;
-                this.age = 0;
+            this.normalize();
+            this.x *=length;
+            this.y *=length;
+            return this;
         };
-        Particle.prototype.update = function(deltaTime) {
+        Point.prototype.normalize = function() {
+            var length = this.length();
+            this.x /= length;
+            this.y /= length;
+            return this;
+        };
+        return Point;
+      })();
+      var Particle = (function() {
+          function Particle() {
+            this.position = new Point();
+            this.velocity = new Point();
+            this.acceleration = new Point();
+            this.age = 0;
+          }
+          Particle.Prototype.initialize = function(x, y, dx, dy) {
+            this.position.x = x;
+            this.position.y = y;
+            this.velocity.x = dx;
+            this.velocity.y = dy;
+            this.acceleration.x = dx * settings.particles.effect;
+            this.acceleration.y = dy * settings.particles.effect;
+            this.age = 0;
+         };
+         Particle.prototype.update = function(deltaTime) {
             this.position.x += this.velocity.x * deltaTime;
             this.position.y += this.velocity.y * deltaTime;
             this.velocity.x += this.acceleration.x * deltaTime;
             this.velocity.y += this.acceleration.y * deltaTime;
             this.age += deltaTime;
-        };
-        Particle.prototype.draw = function(context, image) {
+         };
+         Particle.prototype.draw = function(context, image) {
             function ease(t) {
                 return --t * t * t + 1;
             }
@@ -122,8 +122,8 @@
             );
         };
         return Particle;
-    })();
-    var ParticlePool = (function() {
+      })();
+      var ParticlePool = (function() {
         var particles,
           firstActive = 0;
           firstFree = 0,
